e21efef5720f20aa127abfa63ec4420ef4d9e0a8
John Evans <john.evans@digital.justice.gov.uk>
2020-11-24 15:29:05 +0000

    ⬆️ Upgrade to latest version of hmpps plugin (#22)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.1.2"
+    id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.0.0"
     kotlin("plugin.spring") version "1.4.10"
     id("org.unbroken-dome.xjc") version "2.0.0"
     id("org.owasp.dependencycheck") version "6.0.3"
