8f4eb3bf4c7cb349575647904ac40b1cbf25358d
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2024-09-23 13:24:25 +0100

    PIC-4149 environment variables use - instead of _ for consistency (#205)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/WebServiceSecurityConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/WebServiceSecurityConfig.kt
@@ -18,8 +18,8 @@ class WebServiceSecurityConfig(
     @Value("\${ws-sec.response-encrypt-actions}") private val responseActions: String,
     @Value("\${ws-sec.response-signature-parts}") private val responseSignatureParts: String,
     @Value("\${ws-sec.response-encryption-parts}") private val responseEncryptionParts: String,
-    @Value("\${trusted_cert_alias_name}") private val trustedCertAliasName: String,
-    @Value("\${private_key_alias_name}") private val privateKeyAliasName: String,
+    @Value("\${trusted-cert-alias-name}") private val trustedCertAliasName: String,
+    @Value("\${private-key-alias-name}") private val privateKeyAliasName: String,
     @Value("\${ws-sec.keystore-file-path}") private val keystoreFilePath: String,
     @Value("\${ws-sec.encryption-sym-algorithm}") private val encryptionAlgorithm: String,
 ) {
