4c5f25a5f15b829c842516e38b184aa07ba5f645
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-06-10 15:53:40 +0100

    Update security config

--- src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/SecurityConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/SecurityConfig.kt
@@ -8,8 +8,13 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 class SecurityConfig {
     @Bean
+<<<<<<< HEAD
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http
+=======
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+>>>>>>> aceada2 (Add swagger to crime-portal-gateway)
             .csrf { it.disable() }
             .authorizeHttpRequests { http ->
                 http.requestMatchers(
@@ -21,9 +26,14 @@ class SecurityConfig {
                 ).permitAll()
             }
             .build()
+<<<<<<< HEAD
 <<<<<<< HEAD:src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/InfoSecurityConfig.kt
 }
 =======
     }
 }
 >>>>>>> aceada2 (Add swagger to crime-portal-gateway):src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/SecurityConfig.kt
+=======
+    }
+}
+>>>>>>> aceada2 (Add swagger to crime-portal-gateway)
