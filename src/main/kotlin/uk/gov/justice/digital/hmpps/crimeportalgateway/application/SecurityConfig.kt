d752334a1c93753eb64deee770679f021baba790
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-06-10 15:45:08 +0100

    Add swagger to crime-portal-gateway
    This will show the endpoints available within the service.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/SecurityConfig.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.hmpps.crimeportalgateway.application
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { http ->
+                http.requestMatchers(
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/info/**",
+                    "/health/**",
+                    "/api-docs/**",
+                ).permitAll()
+            }
+            .build()
+<<<<<<< HEAD:src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/InfoSecurityConfig.kt
+}
+=======
+    }
+}
+>>>>>>> aceada2 (Add swagger to crime-portal-gateway):src/main/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/SecurityConfig.kt
